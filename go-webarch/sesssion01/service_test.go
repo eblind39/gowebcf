2a18e71b310b9dda73228e6284939e8fc59d554f
Ernesto Gutierrez <ergutierrez39@gmail.com>
2022-07-24 13:27:26 -0600

    architecture testing

---
+++ go-webarch/sesssion01/service_test.go
@@ -0,0 +1,33 @@
+package architecture
+
+import (
+	"testing"
+)
+
+// mocking storage
+type Db map[int]Persona
+
+func (m Db) Save(n int, p Persona) {
+	m[n] = p
+}
+func (m Db) Retrieve(n int) (p Persona) {
+	return m[n]
+}
+
+func TestPut(t *testing.T) {
+	mdb := Db{}
+	p := Persona{
+		First: "Steven",
+	}
+
+	psm := NewPersonService(mdb)
+
+	// psm.Put(2, p) // Fail
+	psm.Put(1, p) // Pass
+
+	got := mdb.Retrieve(1)
+
+	if got != p {
+		t.Fatalf("Expected %v, got %v", p, got)
+	}
+}
