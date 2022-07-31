94e051c09cc632610b5d94e1872ce8d22fa20e5f
Ernesto Gutierrez <ergutierrez39@gmail.com>
2022-07-31 12:08:35 -0600

    errors assertion - refactor

--- go-webarch/session04/errors03.go
+++ go-webarch/session04/errors03.go
@@ -12,10 +12,19 @@ func main() {
 
 	// Assertion
 	var perr *os.PathError
-	if errors.As(err, &perr) {
+	if errors.Is(err, os.ErrPermission) && errors.As(err, &perr) {
+		err = fmt.Errorf("You do not have permission to open the file: %w", err, perr.Path)
+		log.Println(err)
+	} else if errors.Is(err, os.ErrNotExist) && errors.As(err, &perr) {
+		err = fmt.Errorf("The file does not exist: %w", err, perr.Path)
+		log.Println(err)
+	} else if errors.As(err, &perr) {
 		fmt.Println("Here is info from patherror", perr.Op, perr.Path)
 		errMsg := fmt.Errorf("Here is the original error %w and the path", err, perr.Path) // Returns the msg in a func
 		fmt.Println(errMsg)
+	} else if err != nil {
+		err = fmt.Errorf("File couldn't be opened: %w", err)
+		log.Println(err)
 	}
 
 	// better coding practice - choose this one
