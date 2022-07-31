a649d5c71d8cb0ee3ae48647a2797e84c0eda1ed
Ernesto Gutierrez <ergutierrez39@gmail.com>
2022-07-31 13:42:30 -0600

    errors assertion - refactor switch

--- go-webarch/session04/errors03.go
+++ go-webarch/session04/errors03.go
@@ -12,6 +12,25 @@ func main() {
 
 	// Assertion
 	var perr *os.PathError
+
+	// better coding practice - choose this one - switch
+	switch {
+	case errors.Is(err, os.ErrPermission) && errors.As(err, &perr):
+		err = fmt.Errorf("You do not have permission to open the file: %w", err, perr.Path)
+		log.Println(err)
+	case errors.Is(err, os.ErrNotExist) && errors.As(err, &perr):
+		err = fmt.Errorf("The file does not exist: %w", err, perr.Path)
+		log.Println(err)
+	case errors.As(err, &perr):
+		fmt.Println("Here is info from patherror", perr.Op, perr.Path)
+		errMsg := fmt.Errorf("Here is the original error %w and the path", err, perr.Path) // Returns the msg in a func
+		fmt.Println(errMsg)
+	case err != nil:
+		err = fmt.Errorf("File couldn't be opened: %w", err)
+		log.Println(err)
+	}
+
+	// better coding practice - choose this one
 	if errors.Is(err, os.ErrPermission) && errors.As(err, &perr) {
 		err = fmt.Errorf("You do not have permission to open the file: %w", err, perr.Path)
 		log.Println(err)
