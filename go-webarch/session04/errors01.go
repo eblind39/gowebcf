fce903b06baac0406a6ee19c5eb689a5ff112386
Ernesto Gutierrez <ergutierrez39@gmail.com>
2022-07-30 19:53:42 -0600

    custom errors

--- go-webarch/session04/errors01.go
+++ go-webarch/session04/errors01.go
@@ -3,7 +3,22 @@ package main
 import (
 	"errors"
 	"fmt"
+	"time"
 )
+
+type ErrFileNotFound struct {
+	Filename string
+	When     time.Time
+}
+
+func (e ErrFileNotFound) Error() string {
+	return fmt.Sprintf("File %s was not found at %v", e.Filename, e.When)
+}
+
+func (e ErrFileNotFound) Is(other error) bool {
+	_, ok := other.(ErrFileNotFound)
+	return ok
+}
 
 var ErrNotExist = fmt.Errorf("File does not exist")
 var ErrUserNotExist = errors.New("User does not exist")
@@ -16,4 +31,13 @@ func main() {
 	} else {
 		fmt.Println("Unknow error")
 	}
+
+	errF := ErrFileNotFound{
+		Filename: "test.txt",
+		When:     time.Now(),
+	}
+
+	fmt.Println(errF)
+
+	fmt.Println(errors.Is(errF, ErrFileNotFound{}))
 }
