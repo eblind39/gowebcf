8c35638785cd7882db1c35a10a6be450b8357843
Ernesto Gutierrez <ergutierrez39@gmail.com>
2022-07-31 15:51:42 -0600

    errors handle once

---
+++ go-webarch/session04/errors05.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"io"
+	"os"
+)
+
+type writeFile struct {
+	f   *os.File
+	err error
+}
+
+func newWriteFile(filename string) writeFile {
+	f, err := os.Create(filename)
+	return writeFile{
+		f:   f,
+		err: err,
+	}
+}
+
+func (w *writeFile) writeString(text string) {
+	if w.err != nil {
+		return
+	}
+
+	_, err := io.WriteString(w.f, text)
+	if err != nil {
+		w.err = err
+	}
+}
+
+func (w *writeFile) Close() {
+	if w.err != nil {
+		return
+	}
+
+	err := w.f.Close()
+	if err != nil {
+		w.err = err
+	}
+}
+
+func (w *writeFile) Err() error {
+	return w.err
+}
+
+func main() {
+	f := newWriteFile("file.txt")
+	f.writeString("Hello World!")
+	f.writeString("More text")
+	f.Close()
+
+	err := f.Err()
+	if err != nil {
+		panic(err)
+	}
+}
