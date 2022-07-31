eb720cf46346a57569adbbbf7f8b8f97274d2545
Ernesto Gutierrez <ergutierrez39@gmail.com>
2022-07-31 12:04:42 -0600

    errors assertion

--- go-webarch/session04/errors03.go
+++ go-webarch/session04/errors03.go
@@ -8,7 +8,15 @@ import (
 )
 
 func main() {
-	f, err := os.Open("rumi.txt")
+	f, err := os.Open("rumi_nf.txt")
+
+	// Assertion
+	var perr *os.PathError
+	if errors.As(err, &perr) {
+		fmt.Println("Here is info from patherror", perr.Op, perr.Path)
+		errMsg := fmt.Errorf("Here is the original error %w and the path", err, perr.Path) // Returns the msg in a func
+		fmt.Println(errMsg)
+	}
 
 	// better coding practice - choose this one
 	if errors.Is(err, os.ErrPermission) {
