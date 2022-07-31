e35856250bdbdfe52650781f53cddaae1406e0da
Ernesto Gutierrez <ergutierrez39@gmail.com>
2022-07-30 19:42:34 -0600

    errors intro

---
+++ go-webarch/session04/errors01.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+var ErrNotExist = fmt.Errorf("File does not exist")
+var ErrUserNotExist = errors.New("User does not exist")
+
+func main() {
+	err := ErrUserNotExist
+
+	if err == ErrUserNotExist {
+		fmt.Println("You need to register first!")
+	} else {
+		fmt.Println("Unknow error")
+	}
+}
