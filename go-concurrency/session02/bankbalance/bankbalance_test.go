6b23e7b67507309128e42b9325151c9025662017
Ernesto Gutierrez <ergutierrez39@gmail.com>
2022-08-19 20:44:12 -0600

    test: bank balance

---
+++ go-concurrency/session02/bankbalance/bankbalance_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"io"
+	"os"
+	"strings"
+	"testing"
+)
+
+func Test_main(t *testing.T) {
+	stdOut := os.Stdout
+
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+	main()
+	_ = w.Close()
+	result, _ := io.ReadAll(r)
+	output := string(result)
+
+	os.Stdout = stdOut
+
+	if !strings.Contains(output, "$55120.00") {
+		t.Error("wrong balance returned")
+	}
+}
