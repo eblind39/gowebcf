21fce5d4c212a30ab2415e34c2c3d49529adfc97
Ernesto Gutierrez <ergutierrez39@gmail.com>
2022-08-25 20:01:35 -0600

    feat: final project - connecting to db

--- go-concurrency/finalproj/cmd/main.go
+++ go-concurrency/finalproj/cmd/main.go
@@ -1,9 +1,22 @@
 package main
 
+import (
+	"database/sql"
+	"log"
+	"os"
+	"time"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
 const webPort = "80"
 
 func main() {
 	// connect to the database
+	db := initDB()
+	db.Ping()
 
 	// create sessions
 
@@ -16,4 +29,48 @@ func main() {
 	// set up email
 
 	// listen for web connection
+}
+
+func initDB() *sql.DB {
+	conn := connectToDB()
+	if conn == nil {
+		log.Panic("Can't connect to database.")
+	}
+
+	return conn
+}
+
+func connectToDB() *sql.DB {
+	counts := 0
+	dsn := os.Getenv("DSN")
+	for {
+		connection, err := openDB(dsn)
+		if err != nil {
+			log.Println("postgres not yet ready...")
+		} else {
+			log.Println("connected to database!")
+			return connection
+		}
+
+		if counts > 10 {
+			return nil
+		}
+
+		log.Println("Backing off for 1 second")
+		time.Sleep(1 * time.Second)
+		continue
+	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
