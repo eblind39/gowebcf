4768b01119bec0668dc4ff2c1ad31597c26dfc53
Ernesto Gutierrez <ergutierrez39@gmail.com>
2022-08-26 19:38:31 -0600

    feat: enable rest of the pages

--- go-concurrency/finalproj/cmd/web/routes.go
+++ go-concurrency/finalproj/cmd/web/routes.go
@@ -16,6 +16,12 @@ func (app *Config) routes() http.Handler {
 
 	// define application routes
 	mux.Get("/", app.HomePage)
+	mux.Get("/login", app.LoginPage)
+	mux.Post("/login", app.PostLoginPage)
+	mux.Get("/logout", app.Logout)
+	mux.Get("/register", app.RegisterPage)
+	mux.Post("/register", app.PostRegisterPage)
+	mux.Get("/activate-account", app.ActivateAccount)
 
 	return mux
 }
