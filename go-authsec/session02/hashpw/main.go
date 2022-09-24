5b444d54819db302c271eef06a0040c5b619af03
Ernesto Gutierrez <ergutierrez39@gmail.com>
2022-09-23 20:59:53 -0600

    feat: auth - parseToken

--- go-authsec/session02/hashpw/main.go
+++ go-authsec/session02/hashpw/main.go
@@ -101,3 +101,22 @@ func createToken(c *UserClaims) (string, error) {
 	}
 	return signedToken, nil
 }
+
+func parseToken(signedToken string) (*UserClaims, error) {
+	t, err := jwt.ParseWithClaims(signedToken, &UserClaims{}, func(t *jwt.Token) (interface{}, error) {
+		if t.Method.Alg() != jwt.SigningMethodHS512.Alg() {
+			return nil, fmt.Errorf("invalid signing algorithm")
+		}
+
+		return key, nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("error in parseToken while parsing token: %w", err)
+	}
+
+	if !t.Valid {
+		return nil, fmt.Errorf("error in parseToken, token is not valid")
+	}
+
+	return t.Claims.(*UserClaims), nil
+}
