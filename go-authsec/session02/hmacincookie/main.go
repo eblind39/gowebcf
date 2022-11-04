3135bd1fe9cd7374b0e4e2656ad9227c585bbeff
Ernesto Gutierrez <ergutierrez39@gmail.com>
2022-11-03 21:08:39 -0600

    feat: auth - hmac in a cookie

---
+++ go-authsec/session02/hmacincookie/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", everything)
+	http.HandleFunc("/submit", submit)
+	http.ListenAndServe(":8080", nil)
+}
+
+func everything(w http.ResponseWriter, r *http.Request) {
+	c, err := r.Cookie("session")
+	if err != nil {
+		c = &http.Cookie{}
+	}
+	html := `<!DOCTYPE html>
+			<html lang="en">
+			  <head>
+				<meta charset="UTF-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<meta http-equiv="X-UA-Compatible" content="ie=edge">
+				<title>HMAC Example</title>
+				<link rel="stylesheet" href="style.css">
+			  </head>
+			  <body>
+				<p>Cookie value: ` + c.Value + `</p>
+				<form action="/submit" method="post">
+					<input type="text" name="email" />
+					<input type="submit" />
+				</form>
+			  </body>
+			</html>`
+
+	io.WriteString(w, html)
+}
+
+func getCode(msg string) string {
+	h := hmac.New(sha256.New, []byte("i love thursdays when it rains 8723 inches"))
+	h.Write([]byte(msg))
+	return fmt.Sprintf("%x", h.Sum(nil))
+}
+
+func submit(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	email := r.FormValue("email")
+	if email == "" {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	code := getCode(email)
+
+	// "hash / message digest / digest / hash value" | "what we stored"
+	c := http.Cookie{
+		Name:  "session",
+		Value: code + "|" + email,
+	}
+
+	http.SetCookie(w, &c)
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
